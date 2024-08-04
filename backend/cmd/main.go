23f297d49b1b90a98622510103ffcbfb8760feb8
Hetsunya <99246859+Hetsunya@users.noreply.github.com>
2024-08-04 16:14:59 +0500

    logs
    Добавил логи от gin

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -5,11 +5,29 @@ import (
 	"log"
 	"manga-reader/backend/internal/routes"
 	_ "manga-reader/docs"
+	"os"
 
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
+	// Создайте папку для логов, если она не существует
+	logDir := "backend/logs"
+	if err := os.MkdirAll(logDir, os.ModePerm); err != nil {
+		log.Fatalf("Failed to create log directory: %v", err)
+	}
+
+	// Создайте файл для логов
+	logFile, err := os.Create(fmt.Sprintf("%s/gin.log", logDir))
+	if err != nil {
+		log.Fatalf("Failed to create log file: %v", err)
+	}
+	defer logFile.Close()
+
+	// Настройте логирование в файл
+	gin.DefaultWriter = logFile
+	gin.DefaultErrorWriter = logFile
+
 	router := gin.Default()
 
 	// Инициализация маршрутов
